f45349b33eb5bcc4fc2d9a2b53e05e83940eabee
Adam-Danish <adamdanish.mdazizi@gmail.com>
2026-08-05 11:18:59 +0800

    Target net8.0 for Docker build compatibility

--- obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59a12ff290150a154806179351eb24fa19da4eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36153b85d933da7e9325e4fcfd32017de6cd2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
